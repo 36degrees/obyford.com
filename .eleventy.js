07eb6a4e4b50ee939a80473e93253e467fad0ebf
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2019-02-03 00:44:49 +0000

    Initial commit of my site, based on 11ty/eleventy-base-blog

---
+++ .eleventy.js
@@ -0,0 +1,91 @@
+const { DateTime } = require("luxon")
+const fs = require("fs")
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+
+module.exports = function(config) {
+  config.addPlugin(pluginRss)
+  config.setDataDeepMerge(true)
+
+  config.addLayoutAlias("base", "layouts/base.njk")
+  config.addLayoutAlias("post", "layouts/post.njk")
+
+  config.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy")
+  })
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  config.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd')
+  })
+
+  // Get the first `n` elements of a collection.
+  config.addFilter("head", (array, n) => {
+    if( n < 0 ) {
+      return array.slice(n)
+    }
+
+    return array.slice(0, n)
+  })
+
+  config.addPassthroughCopy("img")
+  config.addPassthroughCopy("css")
+
+  /* Markdown Plugins */
+  let markdownIt = require("markdown-it")
+  let markdownItAnchor = require("markdown-it-anchor")
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true
+  }
+  let opts = {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  }
+
+  config.setLibrary("md", markdownIt(options)
+    .use(markdownItAnchor, opts)
+  )
+
+  config.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html')
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404)
+          res.end()
+        })
+      }
+    }
+  })
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  }
+}
---
+++ posts/hello-world-again.md
@@ -0,0 +1,11 @@
+---
+title: Hello world (again).
+description: I thought it was time I started blogging again.
+date: 2019-02-03
+---
+
+<p class="paragraph--lead">I thought it was about time I started blogging again.</p>
+
+Let's see if I can actually turn this into a habit and post more than once a year.
+
+This is probably the happiest I've been with anything I've designed in a while, at least. So I've got that going for me, which is nice.
